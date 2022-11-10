b019809a81214086e424779804f0f06fcc4f279f
cducsdcse <q4dong@ucsd.edu>
2022-11-10 11:16:58 -0800

    bash script

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,12 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testLE1(){
+    assertEquals(5,5);
+  }
+  
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,34 @@
 # Create your grading script here
 
-set -e
+#set -e
 
 rm -rf student-submission
 git clone $1 student-submission
+
+FILE1=ListExamples.java
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+
+cp TestListExamples.java student-submissions/
+cd student-submission
+
+if [ -f "$FILE1" ]
+then 
+    echo "$FILE1 exists."
+else 
+    echo "$FILE1 not found."
+    exit 1
+fi
+
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]]
+then 
+    echo "Compile Succeeded."
+else
+    echo "Compile Failed."
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 2> out-err.txt
+cat out-err.txt
 
