219617880f3f80ff4601b9ddc79f2ccf5453756e
cducsdcse <q4dong@ucsd.edu>
2022-11-10 13:37:29 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
 
 
 public class TestListExamples {
@@ -8,5 +9,13 @@ public class TestListExamples {
   public void testLE1(){
     assertEquals(5,5);
   }
-  
+
+  @Test 
+  public void testFilter() {
+    String[] str = {"abc","abcd","edcba","a"};
+    List<String> lst = Arrays.asList(str);
+    String[] str2 ={"abcd","edcba"};
+    List<String> exp = Arrays.asList(str2);
+    assertEquals(exp, ListExamples.filter(lst,new LongerThan3()));
+}
 }
